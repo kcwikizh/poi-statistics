892831a3d48b4bfb7f0687bd60d872cac0c99128
Gizeta <0w0@gizeta.tk>
2015-08-08 15:56:35 +0900

    fix argument may be too long when staticify

--- scripts/staticify.rb
+++ scripts/staticify.rb
@@ -7,14 +7,15 @@ path = Pathname.new(File.dirname(__FILE__)).realpath.parent
 Mongoid.load!("#{path}/config/mongoid.yml", :production)
 Dir["#{path}/models/*.rb"].each { |file| load file }
 
-list = ['/']
-
-list.push '/construction/'
+list = ['/', '/construction/']
 
 CreateShipRecord.distinct(:shipId).each do |id|
   list.push "/construction/ship/#{URI.escape(KCConstants.ships[id])}.json"
   list.push "/construction/ship/#{URI.escape(KCConstants.ships[id])}.html"
 end
+
+puts `staticify --save -d #{path}/public -p "#{list.join(',')}" #{path}`
+list.clear
 
 map = %Q{
   function() {
@@ -31,12 +32,16 @@ CreateShipRecord.map_reduce(map, reduce).out(inline: 1).each do |item|
   list.push "/construction/recipe/#{item["value"]}.html"
 end
 
-list.push '/drop/'
+puts `staticify --save -d #{path}/public -p "#{list.join(',')}" #{path}`
+list = ['/drop/']
 
 DropShipRecord.distinct(:quest).each do |name|
   list.push "/drop/map/#{URI.escape(name)}.json"
   list.push "/drop/map/#{URI.escape(name)}.html"
 end
+
+puts `staticify --save -d #{path}/public -p "#{list.join(',')}" #{path}`
+list.clear
 
 DropShipRecord.distinct(:shipId).each do |id|
   list.push "/drop/ship/#{URI.escape(KCConstants.ships[id])}.json"
