3ae834a928c8026e16a327eea6dbf2b844e68ef3
Gizeta <0w0@gizeta.tk>
2015-09-06 01:56:37 +0900

    add wiki/enemy interface

--- routes/wiki.rb
+++ routes/wiki.rb
@@ -64,3 +64,76 @@ get '/wiki/drop/:mid.?:format?' do
 
   response
 end
+
+get '/wiki/enemy/:mid.json' do
+  map_id = params[:mid].to_i
+
+  enemy_hash = {}
+  DropShipRecord.where(mapId: map_id).distinct(:cellId).sort.each do |cell_id|
+    enemy_hash[KCConstants.cells[map_id][cell_id]] ||= []
+    enemy_hash[KCConstants.cells[map_id][cell_id]].push cell_id
+  end
+
+  map = %Q{
+    function() {
+      emit(this.enemyShips.join('/') + '/' + this.enemyFormation, null);
+    }
+  }
+
+  reduce = %Q{
+    function(key, values) {
+      return null;
+    }
+  }
+
+  enemies = []
+  enemy_hash.each do |enemy_name, cell_id_list|
+    enemy_name =~ /([^\(]+).+?-([A-Z])\)/
+    name = $1; pos = $2
+
+    if map_id > 310
+      enemy_lv_fleets = []
+      [3, 2, 1].each do |i|
+        enemy_fleets = []
+        DropShipRecord.where(:mapId => map_id, :cellId.in => cell_id_list, :enemy => name, :mapLv => i)
+          .map_reduce(map, reduce).out(inline: 1).each do |q|
+            data = q['_id'].split('/').map(&:to_i)
+            enemy_fleets.push({
+              ships: data.take(6),
+              formation: data[-1]
+            })
+          end
+        enemy_lv_fleets.push({
+          level: i,
+          fleets: enemy_fleets
+        })
+      end
+
+      enemies.push({
+        name: name,
+        point: pos,
+        levels: enemy_lv_fleets
+      })
+    else
+      enemy_fleets = []
+      DropShipRecord.where(:mapId => map_id, :cellId.in => cell_id_list, :enemy => name)
+        .map_reduce(map, reduce).out(inline: 1).each do |q|
+          data = q['_id'].split('/').map(&:to_i)
+          enemy_fleets.push({
+            ships: data.take(6),
+            formation: data[-1]
+          })
+        end
+      enemies.push({
+        name: name,
+        point: pos,
+        fleets: enemy_fleets
+      })
+    end
+  end
+
+  content_type :json
+  json_obj = enemies
+  json_obj.to_json
+end
+
