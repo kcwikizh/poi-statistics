3a30dee782e3e3f0cc643d623850729ea27f881c
Gizeta <0w0@gizeta.tk>
2016-02-24 00:39:31 +0900

    update development record model

--- db/migrate/20160220173349_create_development_record.rb
+++ db/migrate/20160220173349_create_development_record.rb
@@ -1,15 +1,15 @@
 class CreateDevelopmentRecord < ActiveRecord::Migration
   def up
     create_table :development_records do |t|
-      t.string  :name,      null: false
-      t.integer :item,      null: false
-      t.integer :cost,      null: false, array: true
-      t.integer :time_no,   null: false
-      t.boolean :success,   null: false
-      t.integer :count,     null: false
-      t.json    :secretary
-      t.json    :hq_lv
-      t.json    :origin
+      t.string  :name,      null: false, default: ""
+      t.integer :item,      null: false, default: 0
+      t.integer :cost,      null: false, default: [0, 0, 0, 0], array: true
+      t.integer :time_no,   null: false, default: 0
+      t.boolean :success,   null: false, default: false
+      t.integer :count,     null: false, default: 0
+      t.json    :secretary, null: false, default: {}
+      t.json    :hq_lv,     null: false, default: {}
+      t.json    :origin,    null: false, default: {}
     end
     add_index :development_records, :name
     add_index :development_records, :item
