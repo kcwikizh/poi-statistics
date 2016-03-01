2ade8e9ae30a9dc0d897195de4e2edf596536cbd
Gizeta <0w0@gizeta.tk>
2016-03-01 13:26:33 +0900

    change kv data helper method

--- helpers/kv_data_helper.rb
+++ helpers/kv_data_helper.rb
@@ -2,8 +2,12 @@ require 'sinatra/base'
 
 module Sinatra
   module KVDataHelper
-    def self.get_or_set_kv_data(key, value = nil)
-      return KVData.where(:key => key).take.value if KVData.exists?(:key => key)
+    def self.get_kv_data(key)
+      return nil unless KVData.exists?(:key => key)
+      KVData.where(:key => key).take.value
+    end
+    
+    def self.set_kv_data(key, value = nil)
       value = block_given? ? yield : value
       KVData.create(:key => key, :value => value)
       value
