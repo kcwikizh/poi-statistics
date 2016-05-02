c43cdd542435f5f8d61c1a952ed6041fba30e5dc
Gizeta <0w0@gizeta.tk>
2016-05-02 15:10:58 +0900

    fix method missing when use CacheHelper

--- helpers/cache_helpers.rb
+++ helpers/cache_helpers.rb
@@ -5,20 +5,20 @@ module Sinatra
   module CacheHelper
     @@store = MiniCache::Store.new
 
-    def cache_get(key)
+    def self.cache_get(key)
       @@store.get(key)
     end
 
-    def cache_set(key, value = nil)
+    def self.cache_set(key, value = nil)
       @@store.set(key, block_given? ? yield : value)
     end
 
-    def cache_get_or_set(key, value = nil)
+    def self.cache_get_or_set(key, value = nil)
       return @@store.get(key) if @@store.set?(key)
       @@store.set(key, block_given? ? yield : value)
     end
 
-    def cache_clear
+    def self.cache_clear
       @@store.reset
     end
   end
