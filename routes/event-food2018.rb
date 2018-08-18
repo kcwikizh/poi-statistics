4cb7c0de08181e300d130a9153c4e40b98e0b379
Gizeta <i@gizeta.me>
2018-08-18 21:15:03 +0800

    add event/food2018

--- routes/event-food2018.rb
+++ routes/event-food2018.rb
@@ -16,5 +16,17 @@ get '/event-food2018/?' do
       :id => i,
       :data => JSON.parse(get_kv_data("event_food2018_180518_drop_item_#{i}")),
     }},
+    :item_data_180524 => [85, 86, 87, 88].map {|i| {
+      :id => i,
+      :data => JSON.parse(get_kv_data("event_food2018_180524_drop_item_#{i}")),
+    }},
+    :item_data_180601 => [85, 86, 87, 88].map {|i| {
+      :id => i,
+      :data => JSON.parse(get_kv_data("event_food2018_180601_drop_item_#{i}")),
+    }},
+    :item_data_180608 => [85, 86, 87, 88].map {|i| {
+      :id => i,
+      :data => JSON.parse(get_kv_data("event_food2018_180608_drop_item_#{i}")),
+    }},
   }
 end
